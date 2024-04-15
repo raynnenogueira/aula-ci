a6b8f56056e4c35562dd8f448df1418efe661c02
Raynne Filomena Pereira Nogueira <85042179+raynnenogueira@users.noreply.github.com>
2024-04-15 20:38:47 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 import "fmt"
-
-// Função de exemplo para adicionar dois numeros inteiros
+// Função de exemplo para adicionar dois números inteiros
 func Add(a, b int) int {
-  return a + b
+    return a + b
 }
-
 func main() {
-  fmt.Println("A soma de 2 e 3 é:", Add(2, 3))
+    fmt.Println("A soma de 2 e 3 é:", Add(2, 3))
 }
