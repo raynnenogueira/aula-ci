64bb5ea050385f21a051f60dc67d2cc13686ca47
Raynne Filomena Pereira Nogueira <85042179+raynnenogueira@users.noreply.github.com>
2024-04-15 20:39:42 -0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,10 +1,9 @@
 package main
 import "testing"
-
-func TestAdd(t *testing T) { // Função pra testar a função de soma
-  result := Add(2, 3)
-  expected := 5
-  if result != expected {
-    t.Errorf("Add(2, 3) = %d", result, expected)
-  }
+func TestAdd(t *testing.T) {
+    result := Add(2, 3)
+    expected := 5
+    if result != expected {
+        t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+    }
 }
